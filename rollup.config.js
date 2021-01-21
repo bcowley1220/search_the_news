199fbb4f1f32821c297f5b9c212ccc339f6fdbe5
Brendan Cowley <bcowley@fusetg.com>
2021-01-20 22:35:42 -0500

    ready for build

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,9 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
+dotenv.config();
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
@@ -15,6 +16,9 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		replace({
+            KEEP_IT_SECRET: JSON.stringify(process.env.KEEP_IT_SECRET)
+        }),
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
@@ -22,7 +26,8 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+			},
+
 		}),
 
 		// If you have external dependencies installed from
--- src/NewsCard.svelte
+++ src/NewsCard.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import Card from "./Card.svelte";
+  window.process = { env: 'development' }
   let newsSearch;
   let articles = [];
   let request;
@@ -9,13 +10,16 @@
   let cardCounter = 0;
   let newsStories = "";
 
+  const keepItSecret = KEEP_IT_SECRET;
+
+  console.log(process.env.NODE_ENV);
   onMount(
     (request = async () => {
       json = null;
       response = await fetch(
-        `http://newsapi.org/v2/top-headlines?` +
+        `https://newsapi.org/v2/top-headlines?` +
           "country=us&" +
-          "apiKey=24036c93ea52469a83075629f87a04b1"
+          `apiKey=${keepItSecret}`
       );
       json = await response.json();
       console.log(json);
@@ -26,7 +30,7 @@
   let searchRequest = async () => {
     json = null;
     response = await fetch(
-      `http://newsapi.org/v2/everything?q=${newsStories}&apiKey=24036c93ea52469a83075629f87a04b1`
+      `http://newsapi.org/v2/everything?q=${newsStories}&apiKey=${keepItSecret}`
     );
     json = await response.json();
     articles = json.articles;
